7165016d26bef8805b35e0d7c6eccb816aff3218
Денис <99500634+KlaDGl@users.noreply.github.com>
2022-06-02 11:38:25 +0300

    Add files via upload

---
+++ Al LR6.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    const int size = 10;
+    int arr[size] = { 7, 87, 13, 45, 37, 17, 73, 52, 55, 45 };
+
+    cout << "Before sorting: " << endl;
+
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    for (int i = 1; i < size; i++) {
+        int j = i;
+        while (arr[j] < arr[j - 1]) {
+            swap(arr[j], arr[j - 1]);
+            if (--j == 0)
+                break;
+        }
+    }
+
+    cout << endl << "After sorting: " << endl;
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+}
